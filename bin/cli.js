ddaefe359152cca6ff7f73bc1c359e1dedd50858
Oscar Calle <gameplaysoscar@gmail.com>
2021-10-09 22:11:05 -0500

    Added cli.js for public package in npm

---
+++ bin/cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const { execSync } = require('child_process')
+
+const runCommand = command => {
+    try {
+        execSync(`${command}`, { stdio: 'inherit' })
+    } catch (e) {
+        console.log(`Failed to execute ${command}`, e)
+        return false
+    }
+    return true
+}
+
+const repoName = process.argv[2]
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/GuasaPlay/apollo-server-mongodb-starter-kit ${repoName}`
+const installDepsCommand = `cd ${repoName} && npm install`
+
+console.log(`Cloning the repository with name ${repoName}`)
+
+const checkedOut = runCommand(gitCheckoutCommand)
+
+if (!checkedOut) process.exit(-1)
+
+console.log(`Installing dependencies for ${repoName}`)
+const installedDeps = runCommand(installDepsCommand)
+if (!installedDeps) process.exit(-1)
+
+console.log(
+    'Congrutulations! You are ready. Follow the following commands to start'
+)
+console.log(`cd ${repoName} && npm start`)
